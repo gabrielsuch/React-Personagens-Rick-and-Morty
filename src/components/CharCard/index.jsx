bcac5b662ad427b33e1e4cf76baec8283b6412e1
Gabriel <gabriel.such@hotmail.com>
2021-11-03 12:26:52 -0300

    Refactored Code

--- src/components/CharCard/index.jsx
+++ src/components/CharCard/index.jsx
@@ -5,10 +5,8 @@ const CharCard = ({character}) => {
     return(
         <div className="characters">
             <li key={character.id}>
-                {/* CRIAR UMA DIV PARA O STATUS, E SE O PERSONAGEM ESTIVER (ALIVE), ENTÃO COLOCAR O FUNDO VERDE, SE FOR (DEAD), ENTÃO COLOCAR O FUNDO VERMELHO */}
                 <p>{character.name}</p>
                 <img src={character.image} alt={character.name} />
-
                 <div className={
                     character.status === "Alive" ?
                     "alive" :
